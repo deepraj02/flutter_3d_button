ac5d0d2e6db5f8c67e88f8160df207da1477ff15
Deepraj Baidya <deeprajbaidya@gmail.com>
2024-03-23 12:43:58 +0530

    📦 FEAT: initial release

---
+++ lib/constants/consts.dart
@@ -0,0 +1,14 @@
+//Color
+import 'package:flutter/material.dart';
+
+const neuBlack = Colors.black;
+const neuDefault1 = Colors.teal;
+const neuShadow = Color(0xFF080808);
+
+// Double
+const neuBorder = 3.0;
+const neuShadowBlurRadius = 0.0;
+
+//others
+const neuOffset = Offset(4, 4);
+const neuBlurStyle = BlurStyle.solid;
